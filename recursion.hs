29ff1e6a836005088e66af08c9f45a42e2a50988
Manu Phatak <bionikspoon@gmail.com>
2015-12-13 22:48:06 -0600

    more recursion

---
+++ recursion.hs
@@ -0,0 +1,49 @@
+module Recursion where
+
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib x = fib (x-1) + fib (x-2)
+
+altMaximum' :: (Ord a) => [a] -> a
+altMaximum' [] = error "maximum of empty list"
+altMaximum' [x] = x
+altMaximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = altMaximum' xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n element
+    | n <= 0 = []
+    | otherwise = element:replicate' (n-1) element
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' ::  [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+_ `elem'` [] = False
+a `elem'` (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs
